a4f4fd8234a9d4ee1cf8fb397663ffa8ec361533
plezhs <pramasns@gmail.com>
2025-05-08 14:32:22 +0900

    주석처리리

---
+++ config.py
@@ -0,0 +1,4 @@
+class config:
+    def __init__(self):
+        self.gpu = ""
+        self.monitor = 0
--- utils/logger.py
+++ utils/logger.py
@@ -1,7 +1,7 @@
-def log(m, location, time):
+def log(m, location, time):                             #m = 메세지, location = py파일 이름, time = 로그 시간
     with open(f"./data/logs/{time}.log", "a") as f:
         f.write(f"[{time}] {location} - {m}\n")
 
-def log_error(m, location, time):
+def log_error(m, location, time):                       #m = 메세지, location = py파일 이름, time = 로그 시간
     with open(f"./data/logs/errors/{time}.log", "a") as f:
         f.write(f"[{time}] {location} - {m}\n")
